ac0234dbb6bec07bd42669f6eb630ff0b9af57b7
mandeepb <kmandeepb@gmail.com>
2014-12-04 12:41:37 -0800

    updated code to fix NaN

--- morgage-cal.js
+++ morgage-cal.js
@@ -18,26 +18,23 @@ function calculateMorgage(p, r, n) {
 }
 
 //to convert percent to decimal
-function percentToDecimal (percent) {
-  return (percent/12)/100;
+function percentToDecimal(percent) {
+  return (percent / 12) / 100;
 }
 
- //convert years to months
-function yearsToMonths (year) {
+//convert years to months
+function yearsToMonths(year) {
   return (year * 12);
 }
 
 // to retrieve data on page
 function postpayments(payment) {
 
-    var htmlEl = document.getElementById('outMonthly');
-
+  var htmlEl = document.getElementById('outMonthly');
     htmlEl.innerText = "$" + payment;
-
 }
 
 //disable btn on page load
-
 var cost = document.getElementById('value').value;
 var btn = document.getElementById('btnCalculate');
 if (cost == '') {
@@ -45,33 +42,36 @@ if (cost == '') {
   btn.classList.add('disable');
 }
 
-
-// var input_box = document.getElementById("inDown");
-// var input_box = document.getElementsByTagName('input');
+//enable btn when clicking in input field
 var input_box = document.getElementsByClassName('input_box');
-// var input_box = document.querySelectorAll("input[type=text]");
 
-for( var i=0; i < input_box.length; i++) {
-  input_box[i].onclick = function() {
-    console.log('test');
+for (var i = 0; i < input_box.length; i++) {
+  input_box[i].onclick = function () {
     btn.disabled = false;
     btn.classList.remove('disable');
   };
 }
-//enable btn when clicking in input field
-// submitting data on click
 
-btn.onclick = function () {
+// submitting data on click
+btn.onclick = function() {
 
   var cost = document.getElementById('value').value;
+
+  if (cost == '') {
+    alert('Please enter number in the field');
+    return false;
+  }
+
+  if (cost < 0) {
+    alert('Please enter valid number');
+    return false;
+  }
   var downPayment = document.getElementById('inDown').value;
   var interest = document.getElementById('apr').value;
   var term = document.getElementById('period').value;
   var amountBorrowed = cost - downPayment;
 
   var pmt = calculateMorgage(amountBorrowed, interest, term);
+    postpayments(pmt);
 
-  postpayments(pmt);
 };
-
-
